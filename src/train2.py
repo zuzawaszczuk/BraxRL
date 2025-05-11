815dfe413feb71d64b8e0ba6f8daee05f249edeb
zwaszczu <01187298@pw.edu.pl>
2025-05-11 21:44:58 +0200

    train commit

---
+++ src/train2.py
@@ -0,0 +1,70 @@
+import functools
+import jax
+from datetime import datetime
+import matplotlib.pyplot as plt
+from brax import envs
+from brax.io import model
+from brax.io import html
+from brax.training.agents.ppo import train as ppo
+from brax.training.agents.sac import train as sac
+
+env_name = 'ant'  # @param ['ant', 'halfcheetah', 'hopper', 'humanoid', 'humanoidstandup', 'inverted_pendulum', 'inverted_double_pendulum', 'pusher', 'reacher', 'walker2d']
+backend = 'positional'  # @param ['generalized', 'positional', 'spring']
+
+# We determined some reasonable hyperparameters offline and share them here.
+train_fn = {
+'inverted_pendulum': functools.partial(ppo.train, num_timesteps=2_000_000, num_evals=20, reward_scaling=10, episode_length=1000, normalize_observations=True, action_repeat=1, unroll_length=5, num_minibatches=32, num_updates_per_batch=4, discounting=0.97, learning_rate=3e-4, entropy_cost=1e-2, num_envs=2048, batch_size=1024, seed=1),
+'inverted_double_pendulum': functools.partial(ppo.train, num_timesteps=20_000_000, num_evals=20, reward_scaling=10, episode_length=1000, normalize_observations=True, action_repeat=1, unroll_length=5, num_minibatches=32, num_updates_per_batch=4, discounting=0.97, learning_rate=3e-4, entropy_cost=1e-2, num_envs=2048, batch_size=1024, seed=1),
+'ant': functools.partial(ppo.train,  num_timesteps=50_0, num_evals=10, reward_scaling=10, episode_length=1000, normalize_observations=True, action_repeat=1, unroll_length=5, num_minibatches=32, num_updates_per_batch=4, discounting=0.97, learning_rate=3e-4, entropy_cost=1e-2, num_envs=4096, batch_size=2048, seed=1),
+'humanoid': functools.partial(ppo.train,  num_timesteps=50_000_000, num_evals=10, reward_scaling=0.1, episode_length=1000, normalize_observations=True, action_repeat=1, unroll_length=10, num_minibatches=32, num_updates_per_batch=8, discounting=0.97, learning_rate=3e-4, entropy_cost=1e-3, num_envs=2048, batch_size=1024, seed=1),
+'reacher': functools.partial(ppo.train, num_timesteps=50_000_000, num_evals=20, reward_scaling=5, episode_length=1000, normalize_observations=True, action_repeat=4, unroll_length=50, num_minibatches=32, num_updates_per_batch=8, discounting=0.95, learning_rate=3e-4, entropy_cost=1e-3, num_envs=2048, batch_size=256, max_devices_per_host=8, seed=1),
+'humanoidstandup': functools.partial(ppo.train, num_timesteps=100_000_000, num_evals=20, reward_scaling=0.1, episode_length=1000, normalize_observations=True, action_repeat=1, unroll_length=15, num_minibatches=32, num_updates_per_batch=8, discounting=0.97, learning_rate=6e-4, entropy_cost=1e-2, num_envs=2048, batch_size=1024, seed=1),
+'hopper': functools.partial(sac.train, num_timesteps=6_553_600, num_evals=20, reward_scaling=30, episode_length=1000, normalize_observations=True, action_repeat=1, discounting=0.997, learning_rate=6e-4, num_envs=128, batch_size=512, grad_updates_per_step=64, max_devices_per_host=1, max_replay_size=1048576, min_replay_size=8192, seed=1),
+'walker2d': functools.partial(sac.train, num_timesteps=7_864_320, num_evals=20, reward_scaling=5, episode_length=1000, normalize_observations=True, action_repeat=1, discounting=0.997, learning_rate=6e-4, num_envs=128, batch_size=128, grad_updates_per_step=32, max_devices_per_host=1, max_replay_size=1048576, min_replay_size=8192, seed=1),
+'halfcheetah': functools.partial(ppo.train, num_timesteps=50_000_000, num_evals=20, reward_scaling=1, episode_length=1000, normalize_observations=True, action_repeat=1, unroll_length=20, num_minibatches=32, num_updates_per_batch=8, discounting=0.95, learning_rate=3e-4, entropy_cost=0.001, num_envs=2048, batch_size=512, seed=3),
+'pusher': functools.partial(ppo.train, num_timesteps=50_000_000, num_evals=20, reward_scaling=5, episode_length=1000, normalize_observations=True, action_repeat=1, unroll_length=30, num_minibatches=16, num_updates_per_batch=8, discounting=0.95, learning_rate=3e-4,entropy_cost=1e-2, num_envs=2048, batch_size=512, seed=3),
+}[env_name]
+
+max_y = {'ant': 100, 'halfcheetah': 8000, 'hopper': 2500, 'humanoid': 13000, 'humanoidstandup': 75_000, 'reacher': 5, 'walker2d': 5000, 'pusher': 0}[env_name]
+min_y = {'reacher': -100, 'pusher': -150}.get(env_name, 0)
+
+xdata, ydata = [], []
+times = [datetime.now()]
+
+
+
+
+   
+env = envs.get_environment(env_name=env_name, backend=backend)
+make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
+model.save_params('ppo_ant', params)
+
+# print(f'time to jit: {times[1] - times[0]}')
+# print(f'time to train: {times[-1] - times[1]}')
+
+env = envs.create(env_name=env_name, backend=backend)
+inference_fn = make_inference_fn(params)
+
+jit_env_reset = jax.jit(env.reset)
+jit_env_step = jax.jit(env.step)
+jit_inference_fn = jax.jit(inference_fn)
+
+rollout = []
+rng = jax.random.PRNGKey(seed=1)
+state = jit_env_reset(rng=rng)
+for _ in range(1000):
+    rollout.append(state.pipeline_state)
+    act_rng, rng = jax.random.split(rng)
+    act, _ = jit_inference_fn(state.obs, act_rng)
+    state = jit_env_step(state, act)
+
+html_string = html.render(env.sys.tree_replace({'opt.timestep': env.dt}), rollout)
+with open("ant_moving.html", "w", encoding="utf-8") as f:
+    f.write(html_string)
+
+plt.xlim([0, train_fn.keywords['num_timesteps']])
+plt.ylim([min_y, max_y])
+plt.xlabel('# environment steps')
+plt.ylabel('reward per episode')
+plt.plot(xdata, ydata)
+plt.savefig("env_name")
