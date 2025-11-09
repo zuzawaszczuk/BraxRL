0a00364319f823c84b01813d4831c14eb8cd12c9
zuzawaszczuk <zuza.waszczuk@gmail.com>
2025-11-09 01:05:46 +0100

    added distrax to use normal distribution and wrote almost function sample_normal

--- src/SAC/actor.py
+++ src/SAC/actor.py
@@ -1,5 +1,8 @@
 from flax import linen as nn
 import jax.numpy as jnp
+import distrax
+
+PRNGKey = jax.Array
 
 
 class ActorNetwork(nn.Module):
@@ -8,14 +11,39 @@ class ActorNetwork(nn.Module):
     n_actions: int
     fc1_dims: int
     fc2_dims: int
+    reparam_noise: float
+    max_action: int
 
     @nn.compact
     def __call__(self, state: jnp.ndarray) -> jnp.ndarray:
-        x = nn.Dense(self.fc1_dims)(state)
-        x = nn.relu(x)
-        x = nn.Dense(self.fc2_dims)(x)
-        x = nn.relu(x)
+        prob = nn.Dense(self.fc1_dims)(state)
+        prob = nn.relu(prob)
+        prob = nn.Dense(self.fc2_dims)(prob)
+        prob = nn.relu(prob)
 
-        
-        x = nn.Dense(1)(x)
-        return x
+        mu = nn.Dense(self.n_action)(prob)
+        sigma = nn.Dense(self.n_action)(prob)
+
+        sigma = jax.lax.clamp(min=slef.reparam_noise, sigma, max=1)
+
+        return mu, sigma
+
+
+def sample_normal(model: ActorNetwork, state: jnp.ndarray, key: PRNGKey, reparameterize: bool=True) -> jnp.ndarray:
+    mu, sigma = model.apply(state)
+    dist = distrax.Normal(loc=mu, scale=sigma)
+    
+    if reparameterize:
+        actions = dist.sample(seed=key)
+    else:
+        actions = jax.lax.stop_gradient(dist.sample(seed=key))
+
+    action = jax.lax.tanh(actions) * self.max_action
+
+    # pytorch code
+    log_probs = probabilities.log_prob(actions)
+    log_probs -= T.log(1 - action.pow(2) + self.reparam_noise)
+
+    log_probs = log_probs.sum(1, keepdim=True)
+
+    return action, log_probs
